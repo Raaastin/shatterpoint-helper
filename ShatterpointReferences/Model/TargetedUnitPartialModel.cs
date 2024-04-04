bb573e5065c915af55acfad6bde4f9e61b9b83d9
Alex <alexandre.chastaing@non.se.com>
2024-04-04 15:53:13 +0200

    feat: add targeted abilities when attacked

---
+++ ShatterpointReferences/Model/TargetedUnitPartialModel.cs
@@ -0,0 +1,11 @@
+﻿using ShatterpointReferences.Units;
+using ShatterpointReferences.Units.Abilities;
+
+namespace ShatterpointReferences.Model
+{
+    public class TargetedUnitPartialModel
+    {
+        public Unit SelectedUnit { get; set; }
+        public List<Ability> CurrentTargetSynergies { get; set; } = new List<Ability>();
+    }
+}
