0cbdc319c0e98f8bf675636c423b1078af36c803
Alex <alexandre.chastaing@non.se.com>
2024-04-19 11:57:12 +0200

    fix: ui adjustment

--- Shatterpoint.Lib/Services/UnitDataBaseService.cs
+++ Shatterpoint.Lib/Services/UnitDataBaseService.cs
@@ -50,12 +50,13 @@ namespace Shatterpoint.Lib.Services
             UnitList.Add(StarterBoxFactory.Anakin());
 
 #if DEBUG
-            foreach(var unit in UnitList)
+            foreach (var unit in UnitList)
             {
                 unit.PictureUrl = "/img/examples/Untitled.png";
 
                 foreach (var item in unit.Abilities)
                 {
+                    item.Cost = 0;
                     item.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed blandit libero volutpat sed cras ornare. Nulla";
                 }
             }
