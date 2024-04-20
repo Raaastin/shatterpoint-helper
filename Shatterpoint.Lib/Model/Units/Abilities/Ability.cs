2c8a4a34f8a1e2a21bca18a741fd3374fd9d59df
alexandre <alexandre.chastaing@gmail.com>
2024-04-20 12:27:18 +0200

    fix: factory images

--- Shatterpoint.Lib/Model/Units/Abilities/Ability.cs
+++ Shatterpoint.Lib/Model/Units/Abilities/Ability.cs
@@ -5,12 +5,20 @@
     /// </summary>
     public class Ability
     {
+        //public Ability(Unit weilder, string name, AbilityType abilityType, int cost)
+        //{
+        //    Weilder = weilder;
+        //    Name = name;
+        //    Type = abilityType;
+        //    Cost = cost;
+        //}
+
         public Unit Weilder { get; set; }
         public string Name { get; set; }
         public AbilityType Type { get; set; }
         public int Cost { get; set; }
         public string Text { get; set; }
-        public List<Synergy> Synergies { get; set; }
-        public List<Timing> Timing { get; set; }
+        public List<Synergy> Synergies { get; set; } = new List<Synergy>();
+        public List<Timing> Timing { get; set; } = new List<Timing>();
     }
 }
