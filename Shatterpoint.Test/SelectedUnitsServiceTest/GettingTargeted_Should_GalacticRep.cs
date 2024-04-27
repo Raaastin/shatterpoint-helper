16fd853cf71c8a2eedff0520874f24d81e9bbc4e
alexandre <alexandre.chastaing@gmail.com>
2024-04-27 17:50:36 +0200

    test: unit test for galactic republic starter set

--- Shatterpoint.Test/SelectedUnitsServiceTest/GettingTargeted_Should_GalacticRep.cs
+++ Shatterpoint.Test/SelectedUnitsServiceTest/GettingTargeted_Should_GalacticRep.cs
@@ -22,23 +22,96 @@ namespace ShatterpointReference.Test.SelectedUnitsServiceTest
         }
 
         [Fact]
-        public void Contains_ReactiveOrDefensive_abilities_Ahsoka()
+        public void Ahsoka()
         {
             // Act
-            var result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("Ahsoka Tano, Jedi no more"));
-
-            // Arrange
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("Ahsoka Tano, Jedi no more"));
             Assert.Equal(2, result.Count);
+            Assert.Contains("Force Speed", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Coordinated fire", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("Ahsoka Tano, Jedi no more"));
+            Assert.Contains("what's the matter", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("deflect", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
         }
 
         [Fact]
-        public void Contains_ReactiveOrDefensive_abilities_Anakin()
+        public void Anakin()
         {
             // Act
-            var result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("General Anakin Skywalker"));
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("General Anakin Skywalker"));
+            Assert.Contains("Force Jump", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("I'm going to end thi", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("This is where the fun begins", result[2].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Coordinated fire", result[3].Name, StringComparison.CurrentCultureIgnoreCase);
 
-            // Arrange
-            Assert.Equal(2, result.Count);
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("General Anakin Skywalker"));
+            Assert.Contains("Deflect", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Fierce Protector", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void Rex()
+        {
+            // Act
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("CC-7567 Captain Rex"));
+            Assert.Contains("Get a move on", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Defensive Maneuver", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Bring it on", result[2].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Coordinated fire", result[3].Name, StringComparison.CurrentCultureIgnoreCase);
+
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("CC-7567 Captain Rex"));
+            Assert.Contains("Brothers in arms", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Fierce Protector", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void CC501()
+        {
+            // Act
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("CC501st Clone Troopers"));
+            Assert.Contains("defensive maneuver", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("i'm always first, kid", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Bring it on, clankers", result[2].Name, StringComparison.CurrentCultureIgnoreCase);
+
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("CC501st Clone Troopers"));
+            Assert.Contains("Brother in arms", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Fierce Protector", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void Bokatan()
+        {
+            // Act
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("Bo-Katan Kryze"));
+            Assert.Contains("Pride of the Mandalore", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Jet Pack", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Mandalorians are stronger together", result[2].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Mandalore will survive", result[3].Name, StringComparison.CurrentCultureIgnoreCase);
+
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("Bo-Katan Kryze"));
+            Assert.Contains("Some of us serve a higher purpose", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Fierce protector", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void ClanKryzeMandalorian()
+        {
+            // Act
+            var result = selectedUnitsService.GetAbilitiesForTheActiveUnit(db.Get("Clan Kryze Mandalorians"));
+            Assert.Contains("Jet Pack", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Mandalorians are stronger together", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Helmet visor", result[2].Name, StringComparison.CurrentCultureIgnoreCase);
+
+            // Act 2
+            result = selectedUnitsService.GetAbilitiesForTheTargetedUnit(db.Get("Clan Kryze Mandalorians"));
+            Assert.Contains("Fierce protector", result[0].Name, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Contains("Some of us serve a higher purpose", result[1].Name, StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
