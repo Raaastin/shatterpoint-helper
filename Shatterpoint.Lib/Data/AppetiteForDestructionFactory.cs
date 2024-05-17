b2c74c36685b906659751e71177949650ee028e2
alexandre <alexandre.chastaing@gmail.com>
2024-05-18 01:15:24 +0200

    feat: display warband names

--- Shatterpoint.Lib/Data/AppetiteForDestructionFactory.cs
+++ Shatterpoint.Lib/Data/AppetiteForDestructionFactory.cs
@@ -5,7 +5,7 @@ using Shatterpoint.Lib.Units.Abilities;
 namespace Shatterpoint.Lib.Services
 {
     /// <summary>
-    /// Factory for Starter Box
+    /// Factory for Grievous friends
     /// </summary>
     public static class AppetiteForDestructionFactory
     {
